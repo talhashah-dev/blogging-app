dc6fdeeab395f2c9234cbc281eff8fd1e8f451b0
Syed Talha <syedt.in00@gmail.com>
2023-12-28 01:12:54 +0500

    modifications

---
+++ assests/js/firebase.js
@@ -0,0 +1,13 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC3A8dzjuzCobumRpnjWHj5aNbpYTLiKiI",
+    authDomain: "blogging-app-f422b.firebaseapp.com",
+    projectId: "blogging-app-f422b",
+    storageBucket: "blogging-app-f422b.appspot.com",
+    messagingSenderId: "465320944727",
+    appId: "1:465320944727:web:cb36a4d77022b08b6def12",
+    measurementId: "G-3EE8LNWZ9G"
+  };
+
+  // Initialize Firebase
+  const frd = firebase.initializeApp(firebaseConfig);
