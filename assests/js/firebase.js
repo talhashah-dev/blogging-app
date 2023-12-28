bb3842a2ff9e4688b612787b50d9ad5e03624e6e
Syed Talha <syedt.in00@gmail.com>
2023-12-29 00:55:03 +0500

    firebase modefications

--- assests/js/firebase.js
+++ assests/js/firebase.js
@@ -1,13 +1,84 @@
+document.querySelector("form").addEventListener("submit", (event) =>{
+  event.preventDefault()
+})
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyC3A8dzjuzCobumRpnjWHj5aNbpYTLiKiI",
-    authDomain: "blogging-app-f422b.firebaseapp.com",
-    projectId: "blogging-app-f422b",
-    storageBucket: "blogging-app-f422b.appspot.com",
-    messagingSenderId: "465320944727",
-    appId: "1:465320944727:web:cb36a4d77022b08b6def12",
-    measurementId: "G-3EE8LNWZ9G"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 
-  // Initialize Firebase
-  const frd = firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyDjWgkEDZI6dPWbai_OJ6KLKI9A8l83m4I",
+  authDomain: "blogapp-c7488.firebaseapp.com",
+  projectId: "blogapp-c7488",
+  storageBucket: "blogapp-c7488.appspot.com",
+  messagingSenderId: "889483622671",
+  appId: "1:889483622671:web:8eef199be7330f2418667a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+
+//----- New Registration code start
+// document.getElementById("signup").addEventListener("click", function() {
+//   let email = document.getElementById("email").value;
+//   let password = document.getElementById("password").value;
+
+//   createUserWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//       const user = userCredential.user;
+//       console.log(user);
+//       alert("Registration successfully!!");
+//     })
+//     .catch((error) => {
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       console.log(errorMessage);
+//       alert(error);
+//     });
+// });
+//----- End
+
+// //----- Login code start
+document.getElementById("login").addEventListener("click", function () {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      alert(user.email + " Login successfully!!!");
+      document.getElementById("logout").style.display = "block";
+      location.replace("../dashboard.html")
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      alert(errorMessage);
+    });
+});
+// //----- End
+
+// //----- Logout code start
+// document.getElementById("logout").addEventListener("click", function () {
+//   signOut(auth)
+//     .then(() => {
+//       // Sign-out successful.
+//       console.log("Sign-out successful.");
+//       alert("Sign-out successful.");
+//       document.getElementById("logout").style.display = "none";
+//     })
+//     .catch((error) => {
+//       // An error happened.
+//       console.log("An error happened.");
+//     });
+// });
+// //----- End
---
+++ assests/js/main.js
@@ -0,0 +1,14 @@
+
+let greetings = document.getElementById("greetings");
+let time = new Date().getHours();
+
+// Time Based Greeting for readers
+if(time >= 5 && time < 12){
+    greetings.innerHTML = "Good Morning Readers!";
+} else if(time > 12 && time < 14){
+    greetings.innerHTML = "Good Noon Readers!";
+} else if(time > 14 && time < 18){
+    greetings.innerHTML = "Good Night Readers!";
+} else {
+    greetings.innerHTML = "Good Night Readers!";
+};
