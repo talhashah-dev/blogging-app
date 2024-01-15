af7c7126466c6148e9fa058f08054fa139452079
Syed Talha <syedt.in00@gmail.com>
2024-01-15 23:28:03 +0500

    modifications

--- assests/js/main.js
+++ assests/js/main.js
@@ -3,14 +3,14 @@ let time = new Date().getHours();
 
 // Time Based Greeting for readers
 if (greetings) {
-  if (time <= 5 && time < 12) {
-    greetings.innerHTML = "Good Morning Readers!";
-  } else if (time = 12 && time < 14) {
-    greetings.innerHTML = "Good Noon Readers!";
-  } else if (time > 14 && time < 18) {
-    greetings.innerHTML = "Good Evening Readers!";
+  if (time >= 5 && time < 12) {
+    greetings.innerHTML = "Good Morning Reader!";
+  } else if (time >= 12 && time < 14) {
+    greetings.innerHTML = "Good Noon Reader!";
+  } else if (time >= 14 && time < 18) {
+    greetings.innerHTML = "Good Evening Reader!";
   } else {
-    greetings.innerHTML = "Good Night Readers!";
+    greetings.innerHTML = "Good Night Reader!";
   }
 }
-
+  
